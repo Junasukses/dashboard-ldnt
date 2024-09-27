f9bd2864e86c05a961cad8be913b60738b7f547d
rifyal31 <rifyal31@gmail.com>
2024-09-28 05:26:22 +0700

    fix: layout

--- src/views/PosLandingView.vue
+++ src/views/PosLandingView.vue
@@ -6,6 +6,10 @@ import {
   mdiAccountGroupOutline,
   mdiCash,
   mdiListBoxOutline,
+  mdiFunction,
+  mdiBarcode,
+  mdiLogout,
+  mdiClose,
   mdiMinus,
   mdiPlus,
   mdiSaleOutline,
@@ -78,12 +82,12 @@ function newDataItem(newData) {
   if (isData.length === 0) {
     item.group_data = [
       ...item.group_data,
-      { ...newData, qty: 1, disc_percent: 0, disc_amount: 0, sub_total: newData.price }
+      { ...newData, qty: 1, disc_percent: 0, disc_amount: 0, subtotal: newData.price }
     ]
   } else {
     item.group_data = item.group_data.map((dt) => {
       if (dt.id === newData.id) {
-        return { ...dt, qty: dt.qty + 1, sub_total: dt.price * (dt.qty + 1) }
+        return { ...dt, qty: dt.qty + 1, subtotal: dt.price * (dt.qty + 1) }
       } else {
         return dt
       }
@@ -100,24 +104,25 @@ function onEnterBarcode(data) {
 }
 
 const grand_total = computed(() => {
-  const total = item.group_data.reduce((a, b) => a + Number(b.sub_total), 0)
+  const total = item.group_data.reduce((a, b) => a + Number(b.subtotal), 0)
   data.netto = parseFloat(total)
   return data.netto
 })
 
-const amt_disc = computed(() => {
-  const total = item.group_data.reduce((a, b) => a + Number(b.disc_amount), 0)
-  data.amt_disc = parseFloat(total)
-  return data.amt_disc
-})
+const resetAll = () => {
+  let result = confirm('Lanjutkan reset data ?')
+  if (result) {
+    item.group_data = []
+  }
+}
 
 function changeQty(type, id) {
   item.group_data = item.group_data.map((dt) => {
     if (dt.id === id) {
       if (type === 'plus') {
-        return { ...dt, qty: dt.qty + 1, sub_total: dt.price * (dt.qty + 1) }
+        return { ...dt, qty: dt.qty + 1, subtotal: dt.price * (dt.qty + 1) }
       } else if (type === 'min') {
-        return { ...dt, qty: dt.qty - 1, sub_total: dt.price * (dt.qty - 1) }
+        return { ...dt, qty: dt.qty - 1, subtotal: dt.price * (dt.qty - 1) }
       } else {
         return dt
       }
@@ -172,20 +177,51 @@ function deleteItem(id) {
 <template>
   <div class="overflow-hidden lg:overflow-visible">
     <div
-      class="lg:ml-0 pt-14 min-h-screen w-screen transition-position lg:w-auto bg-gray-50 dark:bg-slate-800 dark:text-slate-100"
+      class="lg:ml-0 pt-4 min-h-screen w-screen transition-position lg:w-auto bg-gray-50 dark:bg-slate-800 dark:text-slate-100"
     >
-      <NavBar :menu="menuNavBar" class="ml-60 lg:ml-0 xl:pl-60"> </NavBar>
       <SectionMain class="!py-0 overflow-hidden">
         <div class="grid grid-cols-12 gap-[12px]">
           <div class="col-span-9">
-            <h1 class="!ml-2 !mb-4 !font-bold !text-2xl">Selamat Datang Admin</h1>
+            <h1 class="!ml-2 !font-bold !text-2xl">Selamat Datang Budi Santoso</h1>
+            <h1 class="!ml-2 !font-bold !text-md">Kasir Shift II</h1>
+            <h1 class="!ml-2 !font-bold !text-md">PERSADA POS</h1>
+            <h1 class="!ml-2 !mb-4 !font-semibold !text-sm">
+              Jl. MT. Haryono No.11, Dinoyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65144
+            </h1>
+            <div class="flex flex-row justify-end space-x-2 w-full">
+              <div
+                style="cursor: pointer"
+                class="!justify-end space-x-6 bg-gray-600 hover:bg-gray-700 text-white text-center py-1 px-4 text-[11px] font-semibold rounded-lg flex items-center justify-center max-h-[120px]"
+              >
+                <button class="flex flex-col items-center justify-center bg-red-500 text-white">
+                  <BaseIcon :path="mdiFunction" size="20" />Open Close Daily
+                </button>
+              </div>
+              <div
+                style="cursor: pointer"
+                class="!justify-end space-x-6 bg-purple-600 hover:bg-purple-700 text-white text-center py-1 px-4 text-[11px] font-semibold rounded-lg flex items-center justify-center max-h-[120px]"
+                @click="resetAll()"
+              >
+                <button class="flex flex-col items-center justify-center bg-red-500 text-white">
+                  <BaseIcon :path="mdiClose" size="20" />Reset
+                </button>
+              </div>
+              <div
+                style="cursor: pointer"
+                class="!justify-end space-x-6 bg-red-600 hover:bg-red-700 text-white text-center py-1 px-4 text-[11px] font-semibold rounded-lg flex items-center justify-center max-h-[120px]"
+              >
+                <button class="flex flex-col items-center justify-center bg-red-500 text-white">
+                  <BaseIcon :path="mdiLogout" size="20" />Tutup POS
+                </button>
+              </div>
+            </div>
             <div class="flex flex-wrap items-center space-x-4">
               <button
                 @click="openListItem"
                 class="!text-[#086968] !border-2 !border-[#086968] flex items-center !p-2 rounded-lg !font-semibold shadow-md cursor-pointer duration-150 transition hover:bg-[#086968] hover:!text-white"
               >
                 <BaseIcon :path="mdiListBoxOutline" size="20" />
-                <span class="ml-2">List Item</span>
+                <span class="ml-2">Daftar Item</span>
               </button>
 
               <FieldPopupKode
@@ -210,6 +246,25 @@ function deleteItem(id) {
                     cellClass: ['justify-center', 'bg-gray-50']
                   },
                   {
+                    headerName: 'No',
+                    valueGetter: (p) => p.node.rowIndex + 1,
+                    width: 60,
+                    sortable: false,
+                    resizable: false,
+                    filter: false,
+                    cellClass: ['justify-center', 'bg-gray-50']
+                  },
+                  {
+                    headerName: 'Barcode',
+                    field: 'barcode',
+                    width: 60,
+                    flex: 1,
+                    sortable: true,
+                    resizable: true,
+                    filter: true,
+                    cellClass: ['justify-center', 'bg-gray-50']
+                  },
+                  {
                     headerName: 'Kode',
                     field: 'item_code',
                     width: 60,
@@ -230,14 +285,24 @@ function deleteItem(id) {
                     cellClass: ['justify-center', 'bg-gray-50']
                   },
                   {
-                    headerName: 'Barcode',
-                    field: 'barcode',
+                    headerName: 'Satuan',
+                    field: 'unit',
                     width: 60,
                     flex: 1,
                     sortable: true,
                     resizable: true,
                     filter: true,
                     cellClass: ['justify-center', 'bg-gray-50']
+                  },
+                  {
+                    headerName: 'Harga',
+                    field: 'price',
+                    width: 60,
+                    flex: 1,
+                    sortable: true,
+                    resizable: true,
+                    filter: true,
+                    cellClass: ['justify-end', 'bg-gray-50']
                   }
                 ]"
                 class="!mt-0 hidden"
@@ -265,7 +330,7 @@ function deleteItem(id) {
                 @click="activeTabIndex = 1"
               >
                 <BaseIcon :path="mdiAccountGroupOutline" size="20" />
-                <span class="ml-2">List Member</span>
+                <span class="ml-2">Daftar Member</span>
               </div>
               <div
                 class="flex items-center p-2 rounded-lg font-semibold shadow-md cursor-pointer duration-300 transition"
@@ -277,26 +342,25 @@ function deleteItem(id) {
                 @click="activeTabIndex = 2"
               >
                 <BaseIcon :path="mdiSaleOutline" size="20" />
-                <span class="ml-2">List Diskon</span>
+                <span class="ml-2">Daftar Diskon</span>
               </div>
-              <div
-                class="flex items-center p-2 rounded-lg font-semibold shadow-md cursor-pointer duration-300 transition"
-                :class="
-                  activeTabIndex == 3
-                    ? 'text-white bg-[#086968]'
-                    : 'text-gray-600 hover:bg-gray-100 bg-white'
-                "
-                @click="activeTabIndex = 3"
-              >
-                <BaseIcon :path="mdiCash" size="20" />
-                <span class="ml-2">Pembayaran</span>
+              <div class="flex items-center space-x-6">
+                <FieldX
+                  label="Member"
+                  placeholder="Cari Member"
+                  :value="form.name"
+                  @input="(v) => (form.name = v)"
+                  class="!mt-0"
+                  :check="false"
+                />
               </div>
+              <h2>Member: -</h2>
             </div>
 
             <CardBox class="mt-4">
               <div class="flex items-center justify-between font-semibold mb-6">
                 <div class="flex justify-between items-center space-x-6">
-                  <h2 style="text-wrap: nowrap">Kode Item</h2>
+                  <BaseIcon :path="mdiBarcode" size="35" />
                   <FieldPopupKode
                     ref="barcodeInput"
                     :value="values.barcode"
@@ -327,6 +391,16 @@ function deleteItem(id) {
                         cellClass: ['justify-center', 'bg-gray-50']
                       },
                       {
+                        headerName: 'Barcode',
+                        field: 'barcode',
+                        width: 60,
+                        flex: 1,
+                        sortable: true,
+                        resizable: true,
+                        filter: true,
+                        cellClass: ['justify-center', 'bg-gray-50']
+                      },
+                      {
                         headerName: 'Kode',
                         field: 'item_code',
                         width: 60,
@@ -347,8 +421,8 @@ function deleteItem(id) {
                         cellClass: ['justify-center', 'bg-gray-50']
                       },
                       {
-                        headerName: 'Barcode',
-                        field: 'barcode',
+                        headerName: 'Satuan',
+                        field: 'unit',
                         width: 60,
                         flex: 1,
                         sortable: true,
@@ -357,23 +431,18 @@ function deleteItem(id) {
                         cellClass: ['justify-center', 'bg-gray-50']
                       }
                     ]"
-                    class="!mt-0"
+                    class="!mt-0 !ml-2"
                   />
                 </div>
 
                 <div class="flex items-center space-x-6">
-                  <h2 style="text-wrap: nowrap">Pilih Member</h2>
-                  <FieldX
-                    :value="form.name"
-                    @input="(v) => (form.name = v)"
-                    class="!mt-0"
-                    :check="false"
-                  />
-                </div>
-
-                <div class="flex items-center space-x-6">
-                  <h2 style="text-wrap: nowrap">Grand Total</h2>
-                  <span class="text-2xl text-blue-900">{{ formatNumber(grand_total) }}</span>
+                  <h2 class="!text-md" style="text-wrap: nowrap">Grand Total</h2>
+                  <span
+                    @click="paymentPopup?.open()"
+                    style="cursor: pointer"
+                    class="text-3xl text-blue-900"
+                    >{{ formatNumber(grand_total) }}</span
+                  >
                 </div>
               </div>
 
@@ -387,11 +456,11 @@ function deleteItem(id) {
                     <tr>
                       <th scope="col" class="px-6 py-3 w-[5%]">No</th>
                       <th scope="col" class="px-6 py-3">Nama Item</th>
-                      <th scope="col" class="px-6 py-3">Harga</th>
+                      <th scope="col" class="px-6 py-3 text-right">Harga</th>
                       <th scope="col" class="px-6 py-3">Qty</th>
                       <th scope="col" class="px-6 py-3">Disc (%)</th>
                       <th scope="col" class="px-6 py-3">Disc (Rp)</th>
-                      <th scope="col" class="px-6 py-3">Sub Total</th>
+                      <th scope="col" class="px-6 py-3 text-right">Sub Total</th>
                       <th scope="col" class="px-6 py-3">Action</th>
                     </tr>
                   </thead>
@@ -411,8 +480,8 @@ function deleteItem(id) {
                     >
                       <td class="px-6 py-3">{{ i + 1 }}</td>
                       <td class="px-6 py-3">{{ dt.item_name }}</td>
-                      <td class="px-6 py-3">{{ formatNumber(dt.price) }}</td>
-                      <td class="px-6 py-3">
+                      <td class="px-6 py-3 text-right">{{ formatNumber(dt.price) }}</td>
+                      <td class="px-6 py-3 text-right">
                         <div class="flex gap-2 items-center justify-around">
                           <BaseButton
                             @click="changeQty('min', dt.id)"
@@ -421,7 +490,7 @@ function deleteItem(id) {
                             small
                             :rounded-full="true"
                           />
-                          <span>{{ dt.qty }}</span>
+                          <span class="text-right">{{ dt.qty }}</span>
 
                           <BaseButton
                             @click="changeQty('plus', dt.id)"
@@ -432,9 +501,9 @@ function deleteItem(id) {
                           />
                         </div>
                       </td>
-                      <td class="px-6 py-3">{{ formatNumber(dt.disc_percent) }}</td>
-                      <td class="px-6 py-3">{{ formatNumber(dt.disc_amount) }}</td>
-                      <td class="px-6 py-3">{{ formatNumber(dt.sub_total) }}</td>
+                      <td class="px-6 py-3 text-right">{{ formatNumber(dt.disc_percent) }}</td>
+                      <td class="px-6 py-3 text-right">{{ formatNumber(dt.disc_amount) }}</td>
+                      <td class="px-6 py-3 text-right">{{ formatNumber(dt.subtotal) }}</td>
 
                       <td class="px-6 py-3">
                         <BaseButtons type="justify-center" no-wrap>
@@ -454,7 +523,7 @@ function deleteItem(id) {
           </div>
 
           <div class="row-span-2 col-span-3 text-[12px] h-full">
-            <CardBox class="h-full">
+            <CardBox class="!h-[full]">
               <h1 class="font-bold text-center text-xl">Detail Nota</h1>
               <div class="flex justify-between items-center py-2 border-t border-b my-4">
                 <div>
@@ -464,81 +533,50 @@ function deleteItem(id) {
                   <span>{{ formattedDate }}</span>
                 </div>
               </div>
-              <div class="flex flex-col border-b">
+              <div v-for="(dt, i) in item.group_data" class="flex flex-col border-b">
                 <div class="flex justify-between font-semibold">
-                  <h1>Beras 5Kg</h1>
-                  <h1>60.000</h1>
+                  <h1>{{ dt.item_name }}</h1>
+                  <h1>{{ formatNumber(dt.subtotal) }}</h1>
                 </div>
                 <div class="flex my-2">
-                  <h1 class="mr-2">x1</h1>
-                  <h1>Disc 3.000</h1>
-                </div>
-
-                <div class="flex justify-between font-semibold">
-                  <h1>Minyak Goreng 5L</h1>
-                  <h1>30.000</h1>
-                </div>
-                <div class="flex my-2">
-                  <h1 class="mr-2">x1</h1>
-                  <h1>Disc 600</h1>
-                </div>
-                <div class="flex justify-between font-semibold">
-                  <h1>Detergen Bubuk 1Kg</h1>
-                  <h1>30.000</h1>
-                </div>
-                <div class="flex my-2">
-                  <h1 class="mr-2">x1</h1>
-                </div>
-                <div class="flex justify-between font-semibold">
-                  <h1>Susu UHT 1L</h1>
-                  <h1>80.000</h1>
-                </div>
-                <div class="flex my-2">
-                  <h1 class="mr-2">x4</h1>
-                  <h1>Disc 400</h1>
+                  <h1 class="mr-2">x{{ formatNumber(dt.qty) }}</h1>
+                  <h1>@{{ formatNumber(dt.price) }}</h1>
                 </div>
               </div>
 
               <div class="flex flex-col border-b py-2 space-y-2">
                 <div class="flex justify-between font-semibold">
                   <h1>Sub Total</h1>
-                  <h1>40.000</h1>
+                  <h1>{{ formatNumber(data.netto) }}</h1>
                 </div>
                 <div class="flex justify-between font-semibold">
                   <h1>Discount</h1>
-                  <h1>4.000</h1>
+                  <h1>{{ formatNumber(0) }}</h1>
                 </div>
                 <div class="flex justify-between font-semibold">
                   <h1>Pajak</h1>
-                  <h1>0</h1>
+                  <h1>{{ formatNumber(0) }}</h1>
                 </div>
               </div>
 
-              <div class="flex flex-col py-2 space-y-2">
+              <div class="flex flex-col space-y-2">
                 <div class="flex justify-between font-semibold">
                   <h1>Total</h1>
-                  <h1>236.000</h1>
+                  <h1>{{ formatNumber(data.netto) }}</h1>
                 </div>
 
                 <div class="flex justify-between font-semibold">
                   <h1>Bayar</h1>
-                  <h1>250.000</h1>
+                  <h1>-</h1>
                 </div>
 
                 <div class="flex justify-between font-semibold">
                   <h1>Kembali</h1>
-                  <h1>14.000</h1>
+                  <h1>-</h1>
                 </div>
               </div>
-
-              <BaseButton
-                label="Pembayaran"
-                @click="paymentPopup?.open()"
-                small
-                class="!text-white w-full mt-4 py-2 !bg-[#086968] hover:!bg-[#075e5d]"
-              />
-              <PaymentPopup ref="paymentPopup" :data="data" />
             </CardBox>
+            <PaymentPopup ref="paymentPopup" :data="data" />
           </div>
         </div>
       </SectionMain>
